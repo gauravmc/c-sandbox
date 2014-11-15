991d3b05997f64cf5314be36c0f2bff40e41b0f4
Gaurav Chande <gmail@gauravchande.com>
2014-11-15 15:42:24 -0500

    More scratch code

--- scratch.c
+++ scratch.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
   char str[] = "abc";
 
@@ -11,6 +11,20 @@ int main()
   printf("str is %d\n", str[1]);
   printf("size of foo is %ld\n", sizeof(foo));
   printf("size of str is %ld\n", sizeof(str));
+
+  // print argv
+  printf("first argument %s\n", argv[0]);
+
+  // array of strings
+  char *names[] = {"Gaurav", "Vinit", "Tarun", NULL};
+  printf("size of names array is %ld\n", sizeof(names));
+
+  int i=0;
+  for(i=0; i<4; i++) {
+    printf("name at %d is %s\n", i, names[i]);
+  }
+
+  printf("value of i after the loop %d\n", i);
 
   return 0;
 }
