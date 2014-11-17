b3a77205b02c139c282042e6ab3264e6f132881a
Gaurav Chande <gmail@gauravchande.com>
2014-11-16 21:17:24 -0500

    shell script to test db operations

---
+++ db_test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./db destroy
+./db create
+./db set 1 gaurav gmail@gauravchande.com
+./db set 2 g2 g2@gaurav.com
+./db set 5 g3 g3@gaurav.com
+./db set 25 g4 g4@gaurav.com
+./db list
+./db get 2
+./db del 5
+./db get 5
+./db destroy
