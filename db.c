ed65b7cafa95d167be2c1c1c84e85e26e489761a
Gaurav Chande <gmail@gauravchande.com>
2014-11-16 19:30:12 -0500

    db Connection

--- db.c
+++ db.c
@@ -31,26 +31,43 @@ struct Database {
   struct Record rows[MAX_ROWS];
 };
 
-void Database_create()
+struct Connection {
+  FILE *file;
+  struct Database *db;
+};
+
+struct Connection *Connection_open()
 {
-  struct Database *db = malloc(sizeof(struct Database));
+  struct Connection *conn = malloc(sizeof(struct Connection));
+  if(!conn) die("Memory error");
+
+  conn->db = malloc(sizeof(struct Database));
+  conn->file = fopen("db.dat", "w");
+  if(!conn->file) die("Failed to open the file");
+
+  return conn;
+}
+
+void Database_create(struct Connection *conn)
+{
   int i;
   for(i = 0; i < MAX_ROWS; i++) {
     struct Record record = {.id = i+1, .set = 0};
-    db->rows[i] = record;
+    conn->db->rows[i] = record;
   }
 
-  FILE *file;
-  file = fopen("db.dat", "w");
-  if(!file) die("Failed to open the file");
-
-  int rc = fwrite(db, sizeof(struct Database), 1, file);
+  int rc = fwrite(conn->db, sizeof(struct Database), 1, conn->file);
   if(rc != 1) die("Failed to write database.");
 
-  rc = fflush(file);
+  rc = fflush(conn->file);
   if(rc == -1) die("Cannot flush database.");
 
   printf("Fresh database has been created\n");
+}
+
+void Database_set(int id, const char* name, const char* email)
+{
+  printf("Given record %d, %s, %s\n", id, name, email);
 }
 
 void Database_destroy()
@@ -62,10 +79,22 @@ void Database_destroy()
 int main(int argc, char *argv[])
 {
   if(argc < 2) die("Incorrect API call. API USAGE: db <action> [action params]");
+
+  struct Connection *conn = Connection_open();
+
+  int id = 0;
+  if(argc > 2) {
+    id = atoi(argv[2]);
+    if(id >= MAX_ROWS) printf("Given id=%d is greater than total records.\n", id);
+  }
+
   char *action = argv[1];
 
   if(!strcmp(action, "create")) {
-    Database_create();
+    Database_create(conn);
+  } else if(!strcmp(action, "set")) {
+    if(argc != 5) die("Need id, name, email to set");
+    Database_set(id, argv[3], argv[4]);
   } else if(!strcmp(action, "destroy")) {
     Database_destroy();
   } else {
