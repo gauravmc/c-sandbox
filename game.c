a2d44385cc842d8a5e02ab3fa88738d92615d187
Gaurav Chande <gmail@gauravchande.com>
2014-11-20 01:56:52 +0000

    basic user input setup for the game

---
+++ game.c
@@ -0,0 +1,46 @@
+// A command-line role playing game build using a simple prototype-based object system
+
+// Interface:
+/* Go up: u */ 
+/* Go down: d */
+/* Go left: l */
+/* Go right: r */
+/* Attack / */
+
+#include <stdio.h>
+
+int process_input() {
+  char ch = getchar();
+  getchar(); // skip ENTER
+
+  switch(ch) {
+    case -1:
+      printf("Bye.\n");
+      return 0;
+      break;
+    case 'u':
+      printf("Up\n");
+      break;
+    case 'd':
+      printf("Down\n");
+      break;
+    case 'l':
+      printf("Left\n");
+      break;
+    case 'r':
+      printf("Right\n");
+      break;
+    default:
+      printf("Unrecognized input.\n");
+  }
+
+  return 1;
+}
+
+int main()
+{
+  while(process_input()) {
+  }
+
+  return 0;
+}
