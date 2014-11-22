5612036b281a292089a1507d28a5f2ea832e001e
Gaurav Chande <gmail@gauravchande.com>
2014-11-22 18:35:15 +0000

    types

---
+++ types.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+  /* char *a = "gaurav"; */
+  /* char A = 'A'; */
+  /* char two = 97; */
+  /* char twenty = 20; */
+  /* char two_hundred = 200; */
+  /* int two = 0; */
+
+  int8_t p = 55;
+
+  printf("%d\n", ~p);
+
+  /* printf("%d\n", INT8_MAX); */
+  /* printf("%d\n", INT16_MAX); */
+
+  /* printf("%c\n", a); */
+  /* printf("%d\n", a); */
+  /* printf("%c\n", A); */
+  /* printf("%d\n", A); */
+  /* printf("%c\n", two); */
+  /* printf("%d\n", two); */
+  /* printf("%c\n", twenty); */
+  /* printf("%d\n", twenty); */
+  /* printf("%c\n", two_hundred); */
+  /* printf("%d\n", two_hundred); */
+
+  /* int i = -128; */
+  /* for(i = -128; i < 256; i++) { */
+  /*   printf("%c ", i); */
+  /* } */
+  return 0;
+}
